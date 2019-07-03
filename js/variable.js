993e7cae5c28ce60b739e954c515dbba36dd916e
jlshaw0723 <46547985+jlshaw0723@users.noreply.github.com>
2019-07-02 19:24:54 -0600

    Add files via upload

--- js/variable.js
+++ js/variable.js
@@ -7,3 +7,20 @@ comment
 */
 
 console.log("Hello World!");
+//UI Component (Buttons)
+var input_btn_ViewTracker =
+    document.getElementById("input_btn_View Tracker");
+
+//Example For Now
+var dummy_section = document.getElementById("dummy_section");
+
+input_btn_reading_log.addEventListener("click",function(){
+   console.log("User click on button reading log."); 
+    hide_all_view();
+    dummy_section.classList.remove("d-none");
+    
+});
+
+function hide_all_view(){
+    dummy_section.classList.add("d-none");
+}
